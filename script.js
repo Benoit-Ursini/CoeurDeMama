d0dd22ee94806d7154fb4d7ff99031b334b41f78
Benoit Ursini <benoit.ursini@gmail.com>
2023-04-13 00:53:20 +0100

    Refonte css (margin, padding, container) finalisation services

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+let toggle = document.querySelector(".navtoggle");
+let body = document.querySelector("body");
+
+toggle.addEventListener("click", function () {
+  body.classList.toggle("open");
+});
+
 const ratio = 0.3;
 const options = {
   root: null,
@@ -17,4 +24,18 @@ const handleIntersect = function (entries, observer) {
 const observer = new IntersectionObserver(handleIntersect, options);
 document.querySelectorAll('[class*="reveal"]').forEach(function (visible) {
   observer.observe(visible);
+});
+
+const handleIntersectQuiz = function (entries, observerQuiz) {
+  entries.forEach(function (entry) {
+    if (entry.intersectionRatio > ratio) {
+      entry.target.classList.add("reveal-quiz");
+      observerQuiz.unobserve(entry.target);
+    }
+  });
+};
+
+const observerQuiz = new IntersectionObserver(handleIntersectQuiz, options);
+document.querySelectorAll('[class*="quiz"]').forEach(function (visible) {
+  observerQuiz.observe(visible);
 });
